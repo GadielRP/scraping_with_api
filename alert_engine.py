7576f3d834a47e93be59f4d3c84eb4e616eb80da
GadielRP <163680944+GadielRP@users.noreply.github.com>
2026-04-17 11:51:24 -0600

    finished alert_engine.py refactorization

--- alert_engine.py
+++
@@ -1,5 +0,0 @@
-"""Backward-compatible shim for the Process 1 alert engine."""
-
-from modules.alerts.dual_process.process_1 import AlertEngine, alert_engine
-
-__all__ = ["AlertEngine", "alert_engine"]
