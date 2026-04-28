5370a5da721a11fcf20012cfd6540dc59c13026e
GadielRP <163680944+GadielRP@users.noreply.github.com>
2026-04-28 13:44:36 -0600

    deleted old files

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,11 +1,1 @@
-from .backfill_results import *
-from .extract_historical_results import *
-from .process_null_season import *
-from .sport_seasons_processing import *
-
-__all__ = [
-    "backfill_results",
-    "extract_historical_results",
-    "process_null_season",
-    "sport_seasons_processing"
-]
+"""Utility scripts package."""
